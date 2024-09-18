28bb6bb540315def712b938aa46c730347db7bc8
NHeroOP <neeravtheheroo@gmail.com>
2024-09-18 15:06:18 +0530

    Create site layout

---
+++ src/components/Layout.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import React, { ReactNode } from 'react'
+import Navbar from './NavBar'
+import Footer from './Footer'
+import { useTheme } from 'next-themes'
+
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const { theme } = useTheme()
+
+  return (
+    <div className={`min-h-screen flex flex-col bg-gray-50 dark:bg-gray-900`}>
+      <Navbar  />
+      {children}
+      <Footer />
+    </div>
+  )
+}
