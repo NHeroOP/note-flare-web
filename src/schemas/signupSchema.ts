b796ef95d1edfc934a66553766b3fd0622603f3c
NHeroOP <neeravtheheroo@gmail.com>
2024-09-18 15:07:27 +0530

    Add login and signup schema

---
+++ src/schemas/signupSchema.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+export const usernameValidation = z
+  .string()
+  .min(4, "Username must be atleast 4 characters long")
+  .max(16, "Username must not exceed 16 characters")
+  .regex(/^[a-zA-Z0-9_]+$/, "Username must not contain special characters")
+
+export const signupSchema = z.object({
+  username: usernameValidation,
+  email: z.string().email({message: "Invalid email"}),
+  password: z.string().min(6, {message: "Password must be atleast 6 characters long"})
+})
