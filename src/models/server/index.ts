2d7d44315f31895e82e1a236f65adb59611c5d69
NHeroOP <neeravtheheroo@gmail.com>
2024-09-18 15:11:13 +0530

    Add index file to import collections from one file

---
+++ src/models/server/index.ts
@@ -0,0 +1,11 @@
+import createCommentCollection from "./collections/comment.collection";
+import createLikeCollection from "./collections/like.collection";
+import createNoteCollection from "./collections/note.collection";
+import createUserCollection from "./collections/user.collection";
+
+export {
+  createCommentCollection,
+  createLikeCollection,
+  createNoteCollection,
+  createUserCollection,
+}
