56845e182ccb18a80ed72192761f2508a4eb53b5
NHeroOP <neeravtheheroo@gmail.com>
2024-09-18 15:10:49 +0530

    Names / ids of the collection

---
+++ src/models/name.ts
@@ -0,0 +1,6 @@
+export const db = "note-flare"
+export const noteCollection = "notes"
+export const userCollection = "users"
+export const commentCollection = "comments"
+export const likeCollection = "like"
+export const noteAttachmentBucket = "note-attachments"
