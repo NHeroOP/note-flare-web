2765ac8dfee89d9288585722460fbb2ffe09d852
NHeroOP <neeravtheheroo@gmail.com>
2024-09-19 22:47:19 +0530

    Add constants

---
+++ src/const.ts
@@ -0,0 +1,15 @@
+export const subjects = [
+  { name: 'All Subjects', value: 'all' },
+  { name: 'Mathematics', value: 'mathematics' },
+  { name: 'Physics', value: 'physics' },
+  { name: 'Chemistry', value: 'chemistry' },
+  { name: 'Biology', value: 'biology' },
+  { name: 'Computer Science', value: 'computer-science' },
+  { name: 'English', value: 'English' },
+  { name: 'Hindi', value: 'Hindi' },
+  { name: 'PHE', value: 'PHE' },
+  { name: 'Economics', value: 'Economics' },
+  { name: 'History', value: 'History' },
+  { name: 'Civics', value: 'Civics' },
+  { name: 'Geography', value: 'Geography' },
+]
