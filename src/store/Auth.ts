47ca1af4649b36b980479c918f198145796065f8
NHeroOP <neeravtheheroo@gmail.com>
2024-09-26 22:43:23 +0530

    Update auth store and have authenticated state

--- src/store/Auth.ts
+++ src/store/Auth.ts
@@ -1,129 +1,46 @@
-import { account, OAuthProvider } from "@/models/client/config";
-import { AppwriteException, ID, Models } from "appwrite"
+import axios from "axios";
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
 
 interface IAuthStore {
-  session: Models.Session | null;
-  user: Models.User<{}> | null;
-  jwt: string | null;
+  isAuthenticated: boolean;
   hydrated: boolean;
 
   setHydrated(): void;
+  setAuthenticated(isAuthenticated: boolean): void;
   verifySession(): Promise<void>;
-  login(email: string, password: string): Promise<{
-    sucess: boolean; error?: AppwriteException | undefined | null
-  }>;
-  loginWithGoogle(): Promise<{
-    sucess: boolean; error?: AppwriteException | undefined | null
-  }>;
-  createAccount(name: string, email: string, password: string): Promise<{
-    sucess: boolean; error?: AppwriteException | undefined | null
-  }>;
-  logout(): Promise<void>;
 }
 
 export const useAuthStore = create<IAuthStore>()(
   persist(
     immer((set) => ({
-      session: null,
-      jwt: null,
-      user: null,
+      isAuthenticated: false,
       hydrated: false,
-      
+
       setHydrated() {
-        set({hydrated: true})
+        set({ hydrated: true });
+      },
+
+      setAuthenticated(isAuthenticated) { 
+        set({ isAuthenticated });
       },
 
       async verifySession() {
         try {
-          const session = await account.getSession("current")
-          set({ session })
+          const haveSession = (await axios.get("/api/session/verify-session")).data;
           
-        }
-        catch (error) {
-          console.log(error);
-          
-        }
-      },
-
-      async login(email: string, password: string) { 
-        try {
-          const session = await account.createEmailPasswordSession(email, password)
-          
-          const [user, { jwt }] = await Promise.all([
-            account.get(),
-            account.createJWT(),
-
-          ])
-
-          set({ session, user, jwt })
-          
-          return { sucess: true }
-          
-        }
-        catch (error) {
-          console.log(error);
-          
-          return { sucess: false, error: error instanceof AppwriteException ? error : null }
-          
+          if (haveSession.success) {
+            set({ isAuthenticated: true })
+          } else {
+            set({ isAuthenticated: false })
+          }
+        } catch (err) {
+          console.log(err);
+          set({ isAuthenticated: false })
         }
       },
-      async loginWithGoogle() { 
-        try {
-          await account.createOAuth2Session(
-            OAuthProvider.Google,
-            "http://localhost:3000/home",
-            "http://localhost:3000/login",
-          )
-          
-          const [user, { jwt }, session] = await Promise.all([
-            account.get(),
-            account.createJWT(),
-            account.getSession("current")
-
-          ])
-
-          console.log(user, jwt, session);
-          
-
-          set({ session, user, jwt })
-          
-          return { sucess: true }
-
-          
-        }
-        catch (error) {
-          console.log(error);
-          
-          return { sucess: false, error: error instanceof AppwriteException ? error : null }
-          
-        }
-      },
-      async createAccount(name: string, email: string, password: string) { 
-        try {
-          await account.create(ID.unique(), email, password, name)
-          
-          return { sucess: true }
-          
-        } catch (error) {
-          console.log(error);
-          
-          return { sucess: false, error: error instanceof AppwriteException ? error : null }
-
-        }
-      },
-      async logout() {
-        try {
-          await account.deleteSession("current")
-          set({ session: null, user: null, jwt: null })
-        }
-        catch (error) {
-          console.log(error);
-        }
-       },
-      
+    
     })),
     {
       name: "auth",
