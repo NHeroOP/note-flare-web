f0f6c04a6d50ee6fe63bf0e24b0ee925272c359b
NHeroOP <neeravtheheroo@gmail.com>
2024-09-26 22:40:23 +0530

    Update middleware to check for logged in user if not authenticated redirects to login page only initalise db one time

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,18 +3,45 @@ import type { NextRequest } from 'next/server'
 
 import getOrCreateDB from '@/models/server/dbSetup'
 import getOrCreateStorage from "@/models/server/storageSetup" 
+import { getLoggedInUser } from './models/server/config'
+
+let isDbInitialized = false
+let isStorageInitialized = false
 
 
-export async function middleware(request: NextRequest) {
+export async function middleware(req: NextRequest) {
   
-  await Promise.all([
-    getOrCreateDB(),
-    getOrCreateStorage(),
-  ])
+  if (!isDbInitialized && !isStorageInitialized) {
+    await Promise.all([
+      getOrCreateDB(),
+      getOrCreateStorage()
+    ]);
+    isDbInitialized = true;
+    isStorageInitialized = true;
+  }
+
+  const user = await getLoggedInUser()
   
+  const isLoginPage = req.nextUrl.pathname === "/login"
+  const isRegisterPage = req.nextUrl.pathname === "/register"
+
+  if (!user && !isLoginPage && !isRegisterPage) {  
+
+    const url = req.nextUrl.clone()
+    url.pathname = "/login"
+    return NextResponse.redirect(url)
+  }
+
+  if (user && (isLoginPage || isRegisterPage)) {
+    
+    const url = req.nextUrl.clone()
+    url.pathname = "/home"
+    return NextResponse.redirect(url)
+  }
+
   return NextResponse.next()
 }
- 
+
 export const config = {
   /* Match all request paths except for the ones that start with:
   - api
