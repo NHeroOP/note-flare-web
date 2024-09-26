283063bde08f811b2d27c67ded4f8ad09b172e08
NHeroOP <neeravtheheroo@gmail.com>
2024-09-26 22:35:36 +0530

    update navbar with skeleton loading, session check navbar shows a skeleton until authstate is hydrate navbar verify session and show links accordingly

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@
 
 import { Book, LogIn, LogOut, Moon, Sun } from 'lucide-react'
 import Link from 'next/link'
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { Button } from './ui/button'
 import { useTheme } from 'next-themes'
 import { useRouter } from 'next/navigation'
