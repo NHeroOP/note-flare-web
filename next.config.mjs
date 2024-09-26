98c97b22f3cce7f9236f636a4d94e69808a39841
NHeroOP <neeravtheheroo@gmail.com>
2024-09-26 22:38:41 +0530

    update next config to have images from placehold.co

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,17 @@
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
-  }
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
