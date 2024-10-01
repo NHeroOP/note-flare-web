0e4fa943fa5f7a9f18191f0985933da6fe877575
NHeroOP <neeravtheheroo@gmail.com>
2024-10-01 21:21:25 +0530

    Add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,13 @@
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google"
 import "./globals.css";
+
 import { Layout } from "@/components";
 import ThemeProvider from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
+
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -25,6 +28,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${fontSans.variable} min-h-screen antialiased`}>
         <SpeedInsights />
+        <Analytics />
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
